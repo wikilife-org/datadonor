67c243b6635546815346b11611acb51ed7a01e21
Joaquin Quintas <joako84@gmail.com>
2013-06-17 14:15:09 -0300

    facebook signup

--- urls.py
+++ urls.py
@@ -19,6 +19,32 @@ urlpatterns = patterns('',
                         (r'^admin/', include(admin.site.urls)),
                        )
 
+dev_patterns = patterns('connect.facebook_views',
+                        url(
+                            r'^lazy_decorator_example/$', 'lazy_decorator_example',
+                        name='facebook_lazy_decorator_example'),
+                        url(r'^decorator_example/$', 'decorator_example',
+                            name='facebook_decorator_example'),
+                        url(
+                            r'^decorator_example_scope/$', 'decorator_example_scope',
+                            name='facebook_decorator_example_scope'),
+                        url(r'^wall_post/$',
+                            'wall_post', name='facebook_wall_post'),
+                        url(r'^checkins/$',
+                            'checkins', name='facebook_checkins'),
+                        url(r'^image_upload/$',
+                            'image_upload', name='facebook_image_upload'),
+                        url(r'^canvas/$', 'canvas', name='facebook_canvas'),
+                        url(r'^page_tab/$',
+                            'page_tab', name='facebook_page_tab'),
+                        url(r'^open_graph_beta/$', 'open_graph_beta',
+                            name='facebook_open_graph_beta'),
+                        url(r'^remove_og_share/$', 'remove_og_share',
+                            name='facebook_remove_og_share'),
+                        )
+
+urlpatterns += dev_patterns
+
 print settings.MODE
 if settings.MODE == 'userena':
     urlpatterns += patterns('',
