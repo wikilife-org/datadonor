f41eae498c0eb8a118c51acd6b6986070c7030b1
Joaquin Quintas <joako84@gmail.com>
2013-06-28 14:32:53 -0300

    new datadonor

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 from django.conf import settings
 
-from social.views import home, done, logout, error, form, form2, close_login_popup
+from social.views import home, done, logout, error, form, form2, close_login_popup, greg
 from social.facebook import facebook_view
 from social.vkontakte import vkontakte_view
 from social.odnoklassniki import ok_app, ok_app_info
