421357e2b73ae57b88a66a722b9b4c71c6368384
federico <dd3.argentina@gmail.com>
2013-09-12 11:51:54 -0300

    se implemento la nueva maqueta con la anterior.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,8 @@
 var pieChart;
 var animatedPie;
 var doubleAxisBars;
+var animatedQuarterPie;
+var doubleAxisParams;
 
 window.onload = function () {
   
@@ -8,28 +10,49 @@ window.onload = function () {
   var elements = [
     {
       percentage: 50,
-      color: '#CDBFE0',
-      text: '50 Facebook',
-      x: 150,
-      y: 130
-    },
-    {
-      percentage: 30,
       color: '#6845C7',
-      text: '30 Twitter',
+      text: '50',
       x: 150,
-      y: 150
+      y: 130,
+      image: {
+        path: 'img/iconos/1_face.png',
+        width: '26',
+        height: '20',
+        offsetx: '-13',
+        offsety: '-20'
+      }
     },
     {
-      percentage: 20,
+      percentage: 40,
       color: '#975EE3',
-      text: '20 Linkedin',
+      text: '30',
       x: 150,
-      y: 170
+      y: 150,
+      image: {
+        path: 'img/iconos/3_linke.png',
+        width: '26',
+        height: '20',
+        offsetx: '-13',
+        offsety: '-20'
+      }
+    },
+    {
+      percentage: 10,
+      color: '#CDBFE0',
+      text: '20',
+      x: 150,
+      y: 170,
+      image: {
+        path: 'img/iconos/4_mail.png',
+        width: '26',
+        height: '20',
+        offsetx: '-13',
+        offsety: '-20'
+      }
     }
   ];
   
-  var r_1_1 = Raphael('canvas_1_1', 400, 400);
+  var r_1_1 = Raphael('canvas_1_1', 410, 410);
   animatedPie = new EdAnimatedPie(r_1_1, elements, {
     animationTime: 900,
     easing: '<',
@@ -39,7 +62,21 @@ window.onload = function () {
     centerx: 200,
     centery: 200,
     radius: 100,
-    drawReferences: false
+    borderColor: '#DCDDDD',
+    drawReferences: true,
+    drawCenterImage: true,
+    bubbleColor: '#3F4B5B',
+    text: {
+      color: '#ADB6BF',
+      size: '18'
+    },
+    centerImage: {
+      width: 60,
+      height: 84,
+      x: 168,
+      y: 160,
+      path: 'img/iconos/overall_avg.png'
+    }
   });
   animatedPie.draw();
   
@@ -47,27 +84,48 @@ window.onload = function () {
     {
       percentage: 50,
       color: '#D44B5F',
-      text: '50 Facebook',
+      text: '50',
       x: 150,
-      y: 130
+      y: 130,
+      image: {
+        path: 'img/iconos/1_face.png',
+        width: '26',
+        height: '20',
+        offsetx: '-13',
+        offsety: '-20'
+      }
     },
     {
       percentage: 30,
       color: '#DC6666',
-      text: '30 Twitter',
+      text: '30',
       x: 150,
-      y: 150
+      y: 150,
+      image: {
+        path: 'img/iconos/3_linke.png',
+        width: '26',
+        height: '20',
+        offsetx: '-13',
+        offsety: '-20'
+      }
     },
     {
       percentage: 20,
       color: '#FF836F',
-      text: '20 Linkedin',
+      text: '200',
       x: 150,
-      y: 170
+      y: 170,
+      image: {
+        path: 'img/iconos/4_mail.png',
+        width: '26',
+        height: '20',
+        offsetx: '-13',
+        offsety: '-20'
+      }
     }
   ];
-  var r_1_2 = Raphael('canvas_1_2', 400, 400);
-  animatedPie = new EdAnimatedPie(r_1_2, elements, {
+  var r_1_2 = Raphael('canvas_1_2', 410, 410);
+  animatedPie2 = new EdAnimatedPie(r_1_2, elements, {
     animationTime: 900,
     easing: '<',
     useAnimationDelay: false,
@@ -76,9 +134,23 @@ window.onload = function () {
     centerx: 200,
     centery: 200,
     radius: 100,
-    drawReferences: false
+    borderColor: '#DCDDDD',
+    drawReferences: true,
+    drawCenterImage: true,
+    bubbleColor: '#E56666',
+    text: {
+      color: 'white',
+      size: '18'
+    },
+    centerImage: {
+      width: 67,
+      height: 74,
+      x: 167,
+      y: 160,
+      path: 'img/iconos/your_avg.png'
+    }
   });
-  animatedPie.draw();
+  animatedPie2.draw();
   
   /*********** DOT CHARTS *******************/
   var r_2_1 = Raphael('canvas_2_1', 210, 210);
@@ -94,22 +166,25 @@ window.onload = function () {
       label: '134'
     }
   ], {
-    centerx: 100,
+    centerx: 105,
     centery: 101,
     useAnimationDelay: true,
     animationTime: 900,
     easing: 'bounce',
     fontSize: '40',
-    drawLabels: false
+    drawLabels: false,
+    perimeter: {
+      display: false
+    }
   });
   dotChart.draw();
   
   var r_2_2 = Raphael('canvas_2_2', 210, 210);
   dotChart = new EdDotChart(r_2_2, [
-    {
-      radius: 100,
-      color: '#F6F6F6'
-    },
+//    {
+//      radius: 100,
+//      color: '#F6F6F6'
+//    },
     {
       radius: 80,
       color: '#704DA0'
@@ -119,22 +194,27 @@ window.onload = function () {
       color: '#E26667'
     }
   ], {
-    centerx: 100,
-    centery: 101,
+    centerx: 105,
+    centery: 103,
     useAnimationDelay: true,
     animationTime: 900,
     easing: 'bounce',
     fontSize: '40',
-    drawLabels: false
+    drawLabels: false,
+    perimeter: {
+      display: true,
+      radius: 100,
+      color: '#ECEDED'
+    }
   });
   dotChart.draw();
   
   var r_2_3 = Raphael('canvas_2_3', 210, 210);
   dotChart = new EdDotChart(r_2_3, [
-    {
-      radius: 100,
-      color: '#F6F6F6'
-    },
+//    {
+//      radius: 100,
+//      color: '#F6F6F6'
+//    },
     {
       radius: 60,
       color: '#E26667'
@@ -144,22 +224,27 @@ window.onload = function () {
       color: '#704DA0'
     }
   ], {
-    centerx: 100,
-    centery: 101,
+    centerx: 105,
+    centery: 103,
     useAnimationDelay: true,
     animationTime: 900,
     easing: 'bounce',
     fontSize: '40',
-    drawLabels: false
+    drawLabels: false,
+    perimeter: {
+      display: true,
+      radius: 100,
+      color: '#ECEDED'
+    }
   });
   dotChart.draw();
   
   var r_2_4 = Raphael('canvas_2_4', 210, 210);
   dotChart = new EdDotChart(r_2_4, [
-    {
-      radius: 100,
-      color: '#F6F6F6'
-    },
+//    {
+//      radius: 100,
+//      color: '#F6F6F6'
+//    },
     {
       radius: 40,
       color: '#704DA0'
@@ -169,13 +254,18 @@ window.onload = function () {
       color: '#E26667'
     }
   ], {
-    centerx: 100,
-    centery: 101,
+    centerx: 105,
+    centery: 103,
     useAnimationDelay: true,
     animationTime: 900,
     easing: 'bounce',
     fontSize: '40',
-    drawLabels: false
+    drawLabels: false,
+    perimeter: {
+      display: true,
+      radius: 102,
+      color: '#ECEDED'
+    }
   });
   dotChart.draw();
   
@@ -198,7 +288,7 @@ window.onload = function () {
     },
     {
       percentage: 5,
-      color: '#E56666',
+      color: '#6845C7',
       text: 'Technical institute'
     },
     {
@@ -219,7 +309,7 @@ window.onload = function () {
   ];
   
   var r_3_1 = Raphael('canvas_3_1', 435, 428);
-  animatedPie = new EdQuarterAnimatedPie(r_3_1, elements, {
+  animatedQuarterPie = new EdQuarterAnimatedPie(r_3_1, elements, {
     animationTime: 900,
     easing: '<',
     useAnimationDelay: false,
@@ -230,45 +320,76 @@ window.onload = function () {
     radius: 400,
     drawReferences: false
   });
-  animatedPie.draw();
+  animatedQuarterPie.draw();
   
   /*********** BAR CHARTS *******************/
   var r_4_1 = Raphael('canvas_4_1', 1093, 423);
-  doubleAxisBars = new EdBarChart(r_4_1, {
+  doubleAxisParams = {
     axis: 'both',
     barsAxis: 'x',
-    drawAxis: false,
+    drawAxis: true,
     drawLabels: true,
     elements: [
-      {pos: 20, width: 200, color: '#6C47A7', value: 70},
-      {pos: 230, width: 100, color: '#E56666', value: 120},
-      {pos: 330, width: 100, color: '#6C47A7', value: 80},
-      {pos: 440, width: 200, color: '#6C47A7', value: 140},
-      {pos: 650, width: 200, color: '#6C47A7', value: 200},
-      {pos: 860, width: 200, color: '#6C47A7', value: 100},
+      {pos: 20, width: 200, color: '#6C47A7', value: 70, label: "20"},
+      {pos: 230, width: 100, color: '#E56666', value: 120, label: "40"},
+      {pos: 330, width: 100, color: '#6C47A7', value: 80, label: "50"},
+      {pos: 440, width: 200, color: '#6C47A7', value: 140, label: "60"},
+      {pos: 650, width: 200, color: '#6C47A7', value: 200, label: "70"},
+      {pos: 860, width: 200, color: '#6C47A7', value: 100, label: "80"},
     ],
     xAxis: {
       length: 1093,
+      "stroke-width": 2,
+      color: '#F1F2F2',
       labels: [
-        {pos: 110, text: '15-25', type: 'normal'},
-        {pos: 320, text: '25-35', type: 'normal'},
-        {pos: 540, text: '35-45', type: 'normal'},
-        {pos: 740, text: '45-55', type: 'normal'},
-        {pos: 950, text: '55-65', type: 'normal'}
+        {pos: 110, text: '15-25', type: 'normal', "font-size": 20, "font-family": 'Verdana'},
+        {pos: 320, text: '25-35', type: 'normal', "font-size": 20, "font-family": 'Verdana'},
+        {pos: 540, text: '35-45', type: 'normal', "font-size": 20, "font-family": 'Verdana'},
+        {pos: 740, text: '45-55', type: 'normal', "font-size": 20, "font-family": 'Verdana'},
+        {pos: 950, text: '55-65', type: 'normal', "font-size": 20, "font-family": 'Verdana'}
       ]
     },
     yAxis: {
       length: 423,
+      "stroke-width": 0,
+      name: 'years',
       labels: [
-        {pos: 90, text: '01', width: 1090, type: 'dotted'},
-        {pos: 180, text: '10', width: 1090, type: 'dotted'},
-        {pos: 270, text: '20', width: 1090, type: 'dotted'},
-        {pos: 360, text: '30', width: 1090, type: 'dotted'}
+        {pos: 90, text: '01', width: 1090, type: 'dotted', "stroke-width": 3, color: '#F1F2F2', "text-color": "#ADB6BF"},
+        {pos: 180, text: '10 years avg', width: 1090, type: 'dotted', "stroke-width": 3, color: '#6C53C3', "text-color": "#6C53C3"},
+        {pos: 270, text: '20', width: 1090, type: 'dotted', "stroke-width": 3, color: '#F1F2F2', "text-color": "#ADB6BF"},
+        {pos: 360, text: '30', width: 1090, type: 'dotted', "stroke-width": 3, color: '#F1F2F2', "text-color": "#ADB6BF"}
       ]
     },
     centerx: 30,
     centery: 400,
-    canvasSize: [1093,423]
-  });
+    canvasSize: [1093,425]
+  }
+  doubleAxisBars = new EdBarChart(r_4_1, doubleAxisParams);
   doubleAxisBars.draw();
 };
+
+$(document).ready(function(){
+  $('#your_lvl_c li a').click(function (event) {
+    var pos = $(this).attr('ref');
+    //line = animatedQuarterPie.lines[pos];
+    for(var i in animatedQuarterPie.lines){
+      line = animatedQuarterPie.lines[i];
+      if(pos == i){
+        line.animate({"stroke": '#E56666'}, 500);
+      }else{
+        line.animate({"stroke": animatedQuarterPie.colors[i]}, 500);
+      }
+    }
+  });
+  
+  $('#age_select_form').submit(function(){
+    console.log('form submitted!');
+    var age = $('#age_select_value').val();
+    $('#canvas_4_1').html('');
+    var r_4_1 = Raphael('canvas_4_1', 1093, 423);
+    doubleAxisParams.elements[1].value = 140;
+    doubleAxisBars = new EdBarChart(r_4_1, doubleAxisParams);
+    doubleAxisBars.draw();
+    return false;
+  });
+});
--- static/js/raphael/ed.raphael.js
+++ static/js/raphael/ed.raphael.js
@@ -59,7 +59,10 @@ EdAnimatedPie = function(r, elements, options){
   this.percentages = new Array();
   this.colors = new Array();
   this.lines = new Array();
+  this.labelLocations = new Array();
   this.elements = elements;
+  this.lastX = 0;
+  this.lastY = 0;
   
   this.init = function(){
     this.elements.sort(function(a,b){
@@ -81,6 +84,7 @@ EdAnimatedPie = function(r, elements, options){
     this.r.customAttributes.arc = function (value, total, R) {
       //console.log('color: '+color);
       total = 100;
+      
       color = '#6C47A7';
       var alpha = 360 / total * value,
           a = (90 - alpha) * Math.PI / 180,
@@ -93,6 +97,7 @@ EdAnimatedPie = function(r, elements, options){
       } else {
           path = [["M", _this.options.centerx, _this.options.centery - R], ["A", R, R, 0, +(alpha > 180), 1, x, y]];
       }
+      
       return {path: path};
     };
   }
@@ -100,6 +105,8 @@ EdAnimatedPie = function(r, elements, options){
   this.draw = function(){
     this.drawLines();
     if(this.options.drawReferences) this.drawReferences();
+    if(this.options.drawCenterImage) this.drawCenterImage();
+    this.drawBorder();
   }
   
   this.drawLines = function(){
@@ -117,16 +124,52 @@ EdAnimatedPie = function(r, elements, options){
       line.animate(Raphael.animation({arc: [total+elements[i]['percentage'], 100, this.R]}, this.options.animationTime, this.options.easing).delay(delay));
       total = total + elements[i]['percentage'];
     }
+    console.log(this.labelLocations);
   }
   
   this.drawReferences = function(){
     console.log('drawing references!');
+    var c = 0;
     for(var i in this.elements){
-      this.r.text(elements[i]['x'], elements[i]['y'], elements[i]['text']).attr({
-        fill: elements[i]['color'], 
-        "font-size": this.options.fontSize}
-      ).toFront();  
+      el = elements[i];
+      //console.log('element percentage: '+elements[i]['percentage']);
+      //console.log('total: '+c);
+      var coords = this.getLabelsCoords(el["percentage"], c, this.R, this.options.centerx, this.options.centery);
+      //console.log(coords);
+      this.r.circle(coords[0], coords[1], 32).attr({"fill": this.options.bubbleColor, "stroke-width": 0});
+      this.r.text(coords[0], coords[1]+10, el.text).attr({"font-family": 'Omnes-Semibold', "font-size": this.options.text.size, "fill": this.options.text.color});
+      this.r.image(el.image.path, (coords[0]+parseInt(el.image.offsetx)), (coords[1]+parseInt(el.image.offsety)), el.image.width, el.image.height).toFront();
+      
+      c = c+el["percentage"];
     }
+  }
+  
+  this.getLabelsCoords = function(perc, prevPerc, R, cx, cy){
+    var labelX, labelY;
+    
+    angle = ((perc*360)/100)/2;
+    prevAngle = (prevPerc*360)/100;
+    currentAngle = angle+prevAngle;
+    labelX = cx + (R+this.options.lineWidth+5) * Math.cos(((90-currentAngle)*(Math.PI/180)));
+    labelY = cy - (R+this.options.lineWidth+5) * Math.sin(((90-currentAngle)*(Math.PI/180)));
+    
+    /*console.log('RAD: '+R);
+    console.log('ANGLE: '+angle);
+    console.log('CENTER: '+cx+' - '+cy);
+    console.log('PREV ANGLE: '+prevAngle);
+    console.log('CURRENT ANGLE: '+currentAngle);
+    console.log('COORDS: '+labelX+' - '+labelY);*/
+    
+    return [labelX, labelY];
+  }
+  
+  this.drawCenterImage = function(){
+    this.r.image(this.options.centerImage.path, this.options.centerImage.x, this.options.centerImage.y, this.options.centerImage.width, this.options.centerImage.height).toBack();
+  }
+  
+  this.drawBorder = function(){
+    var param = {stroke: this.options.borderColor, "stroke-width": this.options.lineWidth+8};
+    var line = this.r.path().attr(param).attr({arc: [100, 100, this.R], stroke: this.options.borderColor}).toBack();
   }
   
   this.init();
@@ -188,13 +231,16 @@ EdQuarterAnimatedPie = function(r, elements, options){
       console.log(elements[i]);
       var param = {stroke: elements[i]['color'], "stroke-width": this.options.lineWidth};
       var line = this.r.path().attr(param).attr({arc: [0, 100, this.R], stroke: elements[i]['color']}).toBack();
+      var emptyline = this.r.path().attr(param).attr({arc: [100, 100, this.R], stroke: '#F0EAE6'}).toBack();
+      var whiteline = this.r.path().attr(param).attr({arc: [100, 100, this.R+2], stroke: '#ffffff'}).toBack();
       if(this.options.useAnimationDelay){
         var delay = this.options.animationTime;
       }else{
         delay = 0;
       }
       line.transform('s,-1,1, T,0,0').animate(Raphael.animation({arc: [elements[i]['percentage'], 100, this.R]}, this.options.animationTime, this.options.easing).delay(delay));
-      this.R = this.R - (this.options.lineWidth + 5);
+      this.R = this.R - (this.options.lineWidth + 2);
+      this.lines.push(line);
     }
   }
   
@@ -225,10 +271,10 @@ EdBarChart = function(r, options){
   }
   
   this.draw = function(){
-    if(this.options.drawAxis) this.drawAxis();
-    if(this.options.drawLabels) this.drawLabels();
-    if(this.options.drawValues) this.drawValues();
     this.drawBars();
+    if(this.options.drawValues) this.drawValues();
+    if(this.options.drawLabels) this.drawLabels();
+    if(this.options.drawAxis) this.drawAxis();
   }
   
   this.drawValues = function(){
@@ -255,9 +301,11 @@ EdBarChart = function(r, options){
   
   this.drawXAxis = function(){
     //Build x Axis
+    console.log('DRAWING X AXIS');
     var xTarget = this.options.centerx + this.options.xAxis.length;
     this.r.path("M"+this.options.centerx+" "+this.options.centery+"L"+xTarget+" "+this.options.centery).attr({
-      "stroke-width": 1
+      "stroke-width": this.options.xAxis["stroke-width"],
+      "stroke": this.options.xAxis.color
     });
   }
   
@@ -265,7 +313,7 @@ EdBarChart = function(r, options){
     //Build y Axis
     var yTarget = this.options.centery - this.options.yAxis.length;
     this.r.path("M"+this.options.centerx+" "+this.options.centery+"L"+this.options.centerx+" "+yTarget).attr({
-      "stroke-width": 1
+      "stroke-width": this.options.yAxis["stroke-width"]
     });
   }
   
@@ -274,16 +322,17 @@ EdBarChart = function(r, options){
       for(var i in this.options.xAxis.labels){
         label = this.options.xAxis.labels[i];
         xPos = this.options.centerx + label.pos;
-        this.r.text(xPos, this.options.centery+10, label.text);
+        //this.r.text(xPos, this.options.centery+10, label.text).attr({"font-size":label["font-size"], "font-family": label["font-family"]});
         if(label.type == 'dotted') this.drawDottedLine(label, 'x');
       }
     }
     
     if(this.options.yAxis.labels.length){
+      this.r.text(this.options.centerx+20, 25, this.options.yAxis.name).attr({"font-family": 'Omnes-Semibold', "font-size": '18', "fill": "#ADB6BF"});
       for(var i in this.options.yAxis.labels){
         label = this.options.yAxis.labels[i];
         yPos = this.options.centery - label.pos;
-        this.r.text(this.options.centerx-10, yPos, label.text);
+        this.r.text(this.options.centerx, yPos+15, label.text).attr({"font-family": 'Omnes-Semibold', "font-size": '15', "fill": label['text-color'], 'text-anchor': 'start'});
         if(label.type == 'dotted') this.drawDottedLine(label, 'y');
       }
     }
@@ -294,11 +343,11 @@ EdBarChart = function(r, options){
       var xTarget = this.options.centerx + (label['pos']*this.multiplier);
       var yTarget = this.options.centery - label['width'];
       //this.r.path("M"+this.options.centerx+" "+yTarget+"L"+xTarget+" "+yTarget).attr({"stroke-dasharray": '- ', "stroke-width": 1});
-      this.r.path("M"+xTarget+" "+this.options.centery+"L"+xTarget+" "+yTarget).attr({"stroke-dasharray": '- ', "stroke-width": 1});
+      this.r.path("M"+xTarget+" "+this.options.centery+"L"+xTarget+" "+yTarget).attr({"stroke-dasharray": '.', "stroke-width": label["stroke-width"], "stroke": label["color"], "font-family": 'Omnes-Semibold'}).toBack();
     }else{
       var yTarget = this.options.centery + (label['pos']*this.multiplier);
       var xTarget = this.options.centerx + label['width'];
-      this.r.path("M"+this.options.centerx+" "+yTarget+"L"+xTarget+" "+yTarget).attr({"stroke-dasharray": '- ', "stroke-width": 1});
+      this.r.path("M"+this.options.centerx+" "+yTarget+"L"+xTarget+" "+yTarget).attr({"stroke-dasharray": '.', "stroke-width": label["stroke-width"], "stroke": label["color"], "font-family": 'Omnes-Semibold'}).toBack();
     }
   }
   
@@ -311,7 +360,7 @@ EdBarChart = function(r, options){
       
       var yTarget = this.options.centery + (line['pos']*this.multiplier);
       var xTarget = this.options.centerx + line['width'];
-      this.r.path("M"+this.options.centerx+" "+yTarget+"L"+xTarget+" "+yTarget).attr({"stroke-dasharray": '- ', "stroke-width": 1});
+      this.r.path("M"+this.options.centerx+" "+yTarget+"L"+xTarget+" "+yTarget).attr({"stroke-dasharray": '- ', "stroke-width": 1}).toBack();
     }
   }
   
@@ -321,6 +370,7 @@ EdBarChart = function(r, options){
       console.log('drawing item...');
       var item = this.elements[i];
       if(this.options.barsAxis == 'x'){
+        st = r.set();
         xPos = this.options.centerx + item['pos'];
         this.r.rect(xPos, this.options.centery, item['width'], 0).attr({
           fill: item['color'],
@@ -328,6 +378,9 @@ EdBarChart = function(r, options){
         }).transform('r180').toBack().animate({
           height: item['value']
         }, 1000, 'bounce');
+        
+        this.r.circle(xPos+(item['width']/2), this.options.centery - item['value'], 20).attr({"fill": '#3F4A5A', "stroke-width": 0});
+        this.r.text(xPos+(item['width']/2), this.options.centery - item['value'], item['label']).attr({"fill": '#ffffff', "font-family": 'Omnes-Semibold', "font-size": 20});
       }else{
         yPos = this.options.centery - item['pos'];
         this.r.rect(this.options.centerx, yPos, 0, item['width']).attr({
@@ -366,6 +419,14 @@ EdDotChart = function(r, elements, options){
   
   this.drawCircles = function(){
     var isFirst = true;
+    
+    if(this.options.perimeter.display){
+      p = this.r.circle(this.options.centerx, this.options.centery, this.options.perimeter.radius)
+            .attr(
+              { "stroke-width": 2, "stroke-dasharray": '. ', "stroke": this.options.perimeter.color }
+            );
+    }
+    
     for(var i in this.elements){
       var el = this.elements[i];
       var delay = 0;
@@ -381,7 +442,7 @@ EdDotChart = function(r, elements, options){
               { fill: el.color, "stroke-width": 0 }
             );
       if(isFirst){
-        c.attr({'stroke-width': 5, stroke: '#fff'});
+        c.attr({'stroke-width': 0, stroke: '#fff'});
       }
                       
       c.animate(animation.delay(delay));
@@ -505,6 +566,88 @@ EdSingleBarChart = function(r, elements, options){
   
   this.drawLabels = function(){
     
+  }
+  
+  this.init();
+}
+
+EdAnimatedPieLegacy = function(r, elements, options){
+  var _this = this;
+  this.r = r;
+  this.options = options;
+  this.percentages = new Array();
+  this.colors = new Array();
+  this.lines = new Array();
+  this.labelLocations = new Array();
+  this.elements = elements;
+  this.lastX = 0;
+  this.lastY = 0;
+  
+  this.init = function(){
+    this.elements.sort(function(a,b){
+      if(a.percentage < b.percentage) return 1;
+      if(a.percentage > b.percentage) return -1;
+      return 0;
+    });
+    
+    this.R = this.options.radius;
+    
+    for(var i in elements){
+      this.percentages.push(elements[i]['percentage']);
+      this.colors.push(elements[i]['color'])
+    }
+    this.setCustomAttributes();
+  }
+  
+  this.setCustomAttributes = function(){
+    this.r.customAttributes.arc = function (value, total, R) {
+      //console.log('color: '+color);
+      total = 100;
+      color = '#6C47A7';
+      var alpha = 360 / total * value,
+          a = (90 - alpha) * Math.PI / 180,
+          x = _this.options.centerx + R * Math.cos(a),
+          y = _this.options.centery - R * Math.sin(a),
+          //color = "hsb(".concat(Math.round(R) / 200, ",", value / total, ", .75)"),
+          path;
+      if (total == value) {
+          path = [["M", _this.options.centerx, _this.options.centery - R], ["A", R, R, 0, 1, 1, (_this.options.centerx-0.01), _this.options.centery - R]];
+      } else {
+          path = [["M", _this.options.centerx, _this.options.centery - R], ["A", R, R, 0, +(alpha > 180), 1, x, y]];
+      }
+      
+      return {path: path};
+    };
+  }
+  
+  this.draw = function(){
+    this.drawLines();
+    if(this.options.drawReferences) this.drawReferences();
+  }
+  
+  this.drawLines = function(){
+    console.log('Drawing lines!');
+    var total = 0;
+    for(var i in elements){
+      console.log(elements[i]);
+      var param = {stroke: elements[i]['color'], "stroke-width": this.options.lineWidth};
+      var line = this.r.path().attr(param).attr({arc: [0, 100, this.R], stroke: elements[i]['color']}).toBack();
+      if(this.options.useAnimationDelay){
+        var delay = i*this.options.animationTime;
+      }else{
+        delay = 0;
+      }
+      line.animate(Raphael.animation({arc: [total+elements[i]['percentage'], 100, this.R]}, this.options.animationTime, this.options.easing).delay(delay));
+      total = total + elements[i]['percentage'];
+    }
+    console.log(this.labelLocations);
+  }
+  
+  this.drawReferences = function(){
+    console.log('drawing references!');
+    for(var i in this.elements){
+      console.log('element percentage: '+elements[i]['percentage']);
+    }
   }
   
   this.init();
