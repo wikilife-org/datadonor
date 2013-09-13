f5f02528d8fb592e86333a6771015df311527e47
Joaquin Quintas <joako84@gmail.com>
2013-09-13 15:54:11 -0300

    landing

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 from django.conf import settings
 
-from social.views import home, donate, logout, error, form, form2, close_login_popup, greg, comming, receive_code, wikilife_push, wikilife_pull
+from social.views import home, donate, logout, error, form, form2, close_login_popup, greg, comming, mock, wikilife_push, wikilife_pull
 from social.facebook import facebook_view
 
 admin.autodiscover()
@@ -15,10 +15,9 @@ urlpatterns = patterns('',
     url(r'^error/$', error, name='error'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^form/$', form, name='form'),
-    url(r'^form2/$', form2, name='form2'),
+    url(r'^maqueta/$', mock, name='maqueta'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^fb/', facebook_view, name='fb_app'),
-    url(r'^receive_code/', receive_code, name='receive_code'),
     url(r'^comming/', comming, name='comming'),
     url(r'^close_login_popup/$', close_login_popup, name='login_popup_close'),
     url(r'', include('social_auth.urls')),
