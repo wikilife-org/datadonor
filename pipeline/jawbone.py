2629ec1d89fc91cb5c576c0c3c2c55d150a84a55
Mariano Falcon <mf2286@gmail.com>
2013-10-10 11:35:17 -0300

    jawbone client class added

--- pipeline/jawbone.py
+++ pipeline/jawbone.py
@@ -5,7 +5,9 @@ Jawbone
 from django.utils import simplejson
 from utils.client import oauth_req, dsa_urlopen, build_consumer_oauth_request
 from utils.date_util import get_days_list
+
 import requests
+import json
 
 def jawbone_info(request, *args, **kwargs):
     backend = kwargs.get('backend')
@@ -13,18 +15,73 @@ def jawbone_info(request, *args, **kwargs):
     result = {}
     if backend.name == "jawbone":
         data = kwargs.get('response')
-        import pdb; pdb.set_trace()
-        #fitbit_id = data["id"]
-        #access_token = data["access_token"]
-        #profile = get_user_profile(backend, access_token, fitbit_id)
-        #result.update(profile["user"])
-        #result["profile_img"] = result["avatar150"]
-        #del result["avatar150"]
-        #activity = get_user_activity(backend, access_token, fitbit_id)
-        ##food = get_user_food(backend, access_token, fitbit_id)
-        ##f_dict = dict(activity.items() + food.items())
-        #result.update(activity)
-        #social_user.extra_data.update(result)
-        #social_user.save()
+        jc = JawboneClient(backend, data['access_token'])
+        extra_data = jc.get_user_profile()
+        jc.get_user_trends()
+        jc.get_move()
+        print data
+        
+    
+class JawboneClient():
+    PAGE_SIZE = 25
 
-        return result
+    def __init__(self, backend, access_token):
+        self.backend = backend
+        self.access_token = access_token
+        self.api_host = 'https://jawbone.com/nudge/api/v.1.0/users/@me'
+
+
+    def get_user_profile(self):
+        url =  self.api_host
+        headers = {"Content-type": "application/json", "Authorization": "Bearer %s" % self.access_token}
+        res = requests.get(url , headers=headers)
+        if res.status_code == 200:
+            data = json.loads(res.text)
+        else:
+            data = None        
+        return data
+    
+    def get_user_friends(self):
+        url =  self.api_host + "/friends"
+        headers = {"Content-type": "application/json", "Authorization": "Bearer %s" % self.access_token}
+        res = requests.get(url , headers=headers)
+        if res.status_code == 200:
+            data = json.loads(res.text)
+        else:
+            data = None        
+        return data
+    
+    def get_user_mood(self):
+        url =  self.api_host + "/mood"
+        headers = {"Content-type": "application/json", "Authorization": "Bearer %s" % self.access_token}
+        res = requests.get(url , headers=headers)
+        if res.status_code == 200:
+            data = json.loads(res.text)
+        else:
+            data = None        
+        return data
+    
+    def get_user_trends(self):
+        url =  self.api_host + "/trends"
+        headers = {"Content-type": "application/json", "Authorization": "Bearer %s" % self.access_token}
+        res = requests.get(url , headers=headers)
+        print res.text
+        if res.status_code == 200:
+            data = json.loads(res.text)
+        else:
+            print res.status_code
+            data = None        
+        return data
+    
+
+    def get_move(self, move_xid='BwiqMyh8M'):
+        url =  "https://jawbone.com/nudge/api/v.1.0/moves/%s" % move_xid
+        print url
+        headers = {"Content-type": "application/json", "Authorization": "Bearer %s" % self.access_token}
+        res = requests.get(url, headers=headers) # , headers=headers)
+        if res.status_code == 200:
+            data = json.loads(res.text)
+        else:
+            data = None
+            print res.status_code        
+        return data
