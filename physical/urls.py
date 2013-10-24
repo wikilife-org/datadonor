b79cbf342a51f37550268eb6f0ac9650f7a3e133
Joaquin Quintas <joako84@gmail.com>
2013-10-24 12:06:42 -0300

    Fix phyiscal urls

--- physical/urls.py
+++ physical/urls.py
@@ -7,17 +7,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^physical/exercise/top/', physical_exercise, name='physical_exercise'),
-    url(r'^physical/exercise/user/', physical_user_exercise, name='physical_user_exercise'),
-    url(r'^physical/exercise/steps/distribution', physical_steps_distribution, name='physical_steps_distribution'),
-    url(r'^physical/exercise/miles/distribution', physical_miles_distribution, name='physical_miles_distribution'),
-    url(r'^physical/exercise/hours/distribution', physical_hours_distribution, name='physical_hours_distribution'),
-
     #Physical Mock
-    url(r'^physical/exercise/top/mock/', physical_exercise_mock, name='physical_exercise_mock'),
-    url(r'^physical/exercise/user/mock/', physical_user_exercise_mock, name='physical_user_exercise_mock'),
-    url(r'^physical/exercise/steps/distribution/mock/', physical_steps_distribution_mock, name='physical_steps_distribution_mock'),
-    url(r'^physical/exercise/miles/distribution/mock/', physical_miles_distribution_mock, name='physical_miles_distribution_mock'),
-    url(r'^physical/exercise/hours/distribution/mock/', physical_hours_distribution_mock, name='physical_hours_distribution_mock'),
+    url(r'^exercise/top/mock/', physical_exercise_mock, name='physical_exercise_mock'),
+    url(r'^exercise/user/mock/', physical_user_exercise_mock, name='physical_user_exercise_mock'),
+    url(r'^exercise/steps/distribution/mock/', physical_steps_distribution_mock, name='physical_steps_distribution_mock'),
+    url(r'^exercise/miles/distribution/mock/', physical_miles_distribution_mock, name='physical_miles_distribution_mock'),
+    url(r'^exercise/hours/distribution/mock/', physical_hours_distribution_mock, name='physical_hours_distribution_mock'),
+
+    url(r'^exercise/top/', physical_exercise, name='physical_exercise'),
+    url(r'^exercise/user/', physical_user_exercise, name='physical_user_exercise'),
+    url(r'^exercise/steps/distribution/', physical_steps_distribution, name='physical_steps_distribution'),
+    url(r'^exercise/miles/distribution/', physical_miles_distribution, name='physical_miles_distribution'),
+    url(r'^exercise/hours/distribution/', physical_hours_distribution, name='physical_hours_distribution'),
+
 
 )
