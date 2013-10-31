dd3102a27ea1e745b8ecddd4cf17d6f002a2c770
Joaquin Quintas <joako84@gmail.com>
2013-10-31 14:30:30 -0300

    mood panda

--- health/clients/mood_panda.py
+++ health/clients/mood_panda.py
@@ -9,11 +9,11 @@ http://www.moodpanda.com/api/
 from health.clients.base_device_client import BaseDeviceClient
 from wikilife_utils.date_utils import DateUtils
 from wikilife_utils.formatters.date_formatter import DateFormatter
+from xml.etree import ElementTree
 import requests
 
 
 class MoodPandaClient(BaseDeviceClient):
-    PAGE_SIZE = 25
 
     _api_host = None
     _api_key = None
@@ -21,38 +21,40 @@ class MoodPandaClient(BaseDeviceClient):
     _user_info = None
     _user_id = None
 
-    def __init__(self, api_host, api_key, user_email):
+    def __init__(self, api_host, api_key, user_email=None, user_id=None):
+        
         self._api_host = api_host
         self._api_key = api_key
-        self._user_email = user_email
-        self._user_id = self._get_user_id()
+        
+        if user_id != None:
+            self._user_id = user_id
+        elif user_email != None:
+            self._user_email = user_email
+            self._user_id = self._get_user_id()
+        else:
+            raise Exception("Missing user_email or user_id")
 
-    def _get_user_activity(self, activity_code, date_from, date_to):
-        result = {}
-        result["items"] = None
-        result["activity"] = activity_code
-        result["date_from"] = date_from
-        result["date_to"] = date_to
-
-        uri =  self._user_info[activity_code] 
-
+    def _get_user_mood(self, date_from, date_to):
+        
+        """
+        http://moodpanda.com/api/user/feed/data.ashx?userid=1&from=2013-09-30&to=2013-10-30&format=xml&DateOrder=ASC&key=abc
+        """
+        
+        uri = "/user/feed/data.ashx"
         params = {}
-        params["page"] = 0 
-        params["pageSize"] = self.PAGE_SIZE 
-        params["noEarlierThan"] = DateFormatter.to_date(date_from)
-        params["noLaterThan"] = DateFormatter.to_date(date_to)
-
+        params["from"] = date_from
+        params["to"] = date_to
+        params["userid"] = self._user_id
         response = self._get(uri, params=params)
-        result["items"] = response["items"]
-
-        while (params["page"]+1)*self.PAGE_SIZE < response["size"]:
-            params["page"] += 1
-            response = self._get(uri, params=params)
-            result["items"].extend(response["items"])
-
+        
         return result
 
-    def get_user_profile(self):
+    def _get_user_mood_last_30_days(self):
+        date_to = DateUtils.get_date_utc()
+        date_from = DateUtils.add_days(date_to, -30)
+        return self._get_user_mood(date_from, date_to)
+
+    def _get_user_profile(self):
 
         """
         http://moodpanda.com/api/user/data.ashx?userid=4&format=xml&key=abc
@@ -69,7 +71,8 @@ class MoodPandaClient(BaseDeviceClient):
         params = {}
         params["email"] = self._user_email
  
-        return self._get("/user/getid/data.ashx", params)
+        data = self._get("/user/getid/data.ashx", params)
+        return data[0][0].text
     
     def _get(self, service_uri, params={}):
         """
@@ -80,4 +83,5 @@ class MoodPandaClient(BaseDeviceClient):
         params["key"] = self._api_key
         params["format"] = "xml"
         response = requests.request("GET", url, params=params)
-        return response.json()
+        return ElementTree.fromstring(response.content)
+    
