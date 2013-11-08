1a4d025cbcf7cebd6c4a8b255ae43a4265df6a45
Joaquin Quintas <joako84@gmail.com>
2013-11-08 18:37:29 -0300

    genomics

--- genomics/views.py
+++ genomics/views.py
@@ -47,39 +47,51 @@ def genomics_traits_mock(request):
               },
             {"name": "Hair Curl", 
               "id":7, 
-              "values":[{"name":"Likely blue", "id":"likely_blue", "percentage":60, "user_group":True},
-                        {"name":"Likely brown", "id":"likely_brown","percentage":40, "user_group":False}],
+              "values":[{"name":"Slighty curlier hair", "id":"likely_blue", "percentage":65, "user_group":True},
+                        {"name":"Straigher curlier hair", "id":"likely_brown","percentage":35, "user_group":False}],
               },
-            {"name": "Eye Color", 
+            {"name": "Malaria Resistance", 
               "id":8, 
-              "values":[{"name":"Likely blue", "id":"likely_blue", "percentage":60, "user_group":True},
-                        {"name":"Likely brown", "id":"likely_brown","percentage":40, "user_group":False}],
+              "values":[{"name":"Possibly resistant", "id":"possibly_resistant", "percentage":15, "user_group":True},
+                        {"name":"Resistant", "id":"resistant","percentage":85, "user_group":False}],
               },
-            {"name": "Eye Color", 
+            {"name": "Norovirus Resistance", 
               "id":9, 
-              "values":[{"name":"Likely blue", "id":"likely_blue", "percentage":60, "user_group":True},
-                        {"name":"Likely brown", "id":"likely_brown","percentage":40, "user_group":False}],
+              "values":[{"name":"Not resistant", "id":"not_resistant", "percentage":15, "user_group":True},
+                        {"name":"Resistant", "id":"resistant","percentage":85, "user_group":False}],
               },
-            {"name": "Eye Color", 
+            {"name": "Resistance to HIV/AIDS", 
               "id":10, 
-              "values":[{"name":"Likely blue", "id":"likely_blue", "percentage":60,"user_group":True},
-                        {"name":"Likely brown", "id":"likely_brown","percentage":40, "user_group":False}],
-              },
-            {"name": "Eye Color", 
-              "id":11, 
-              "values":[{"name":"Likely blue", "id":"likely_blue", "percentage":60, "user_group":True},
-                        {"name":"Likely brown", "id":"likely_brown","percentage":40, "user_group":False}],
+              "values":[{"name":"Not resistance", "id":"not_resistance", "percentage":25,"user_group":True},
+                        {"name":"Resistance", "id":"resistance","percentage":75, "user_group":False}],
               }
             ]
     return HttpResponse(simplejson.dumps(data), mimetype="application/json")
 
 def genomics_drugs_mock(request):
-    data = {}
+    data = [{"name":"Alcohol consumption, smoking and risk of esophageal cancer", "id":0, "values":[
+                        {"name":"Typical risk", "id":"typical_risk", "percentage":70},
+                        {"name":"Substantially increased risk", "id":"subs_increased_risk", "percentage":25},
+                        {"name":"Greatly increased risk", "id":"great_typical_risk", "percentage":5},]},
+            {"name":"Oral contraceptives, hormone replacement therapy and risk of venous thromboembolism", "id":1, "values":[
+                        {"name":"Normal", "id":"normal", "percentage":85},
+                        {"name":"Reduced", "id":"reduced", "percentage":11},
+                        {"name":"Unable", "id":"unable", "percentage":4}]
+             }]
     return HttpResponse(simplejson.dumps(data), mimetype="application/json")
 
 def genomics_risks_mock(request):
-    data = {}
+    data = [
+            {"name":"Breast Cancer", "percentages":{"user":60, "all":90}},
+            {"name":"Celiac Disease", "percentages":{"user":8, "all":13}},
+            {"name":"Venous Thromb.", "percentages":{"user":60, "all":20}},
+            {"name":"Melanoma", "percentages":{"user":16, "all":32}},
+            {"name":"Coronary Heart Dis.", "percentages":{"user":6, "all":2}},
+            {"name":"Lung Cancer", "percentages":{"user":1, "all":5}}]
+    
     return HttpResponse(simplejson.dumps(data), mimetype="application/json")
+
+
 
 def genomics_traits(request):
     data = {}
