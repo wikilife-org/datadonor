112343328969a2b6c60618f3f1602437ccbb4e20
Emmanuel <ecarelli@wikilife.com>
2013-11-13 19:02:37 -0300

    fs auth in progress

--- physical/auth_backends/fatsecret.py
+++ physical/auth_backends/fatsecret.py
@@ -28,11 +28,52 @@ class FatsecretAuth(ConsumerBasedOAuth):
     AUTH_BACKEND = FatsecretBackend
     SETTINGS_KEY_NAME = 'FATSECRET_REST_API_ACCESS_KEY'
     SETTINGS_SECRET_NAME = 'FATSECRET_REST_API_SHARED_SECRET'
-    AUTHORIZATION_URL = 'http://www.fatsecret.com/oauth/authorize'
+    AUTHORIZATION_URL = 'http://platform.fatsecret.com/rest/server.api/oauth/authorize'
+    #REQUEST_TOKEN_URL = 'http://platform.fatsecret.com/rest/server.api/oauth/request_token'
     REQUEST_TOKEN_URL = 'http://www.fatsecret.com/oauth/request_token'
-    ACCESS_TOKEN_URL = 'http://www.fatsecret.com/oauth/access_token'
+    ACCESS_TOKEN_URL = 'http://platform.fatsecret.com/rest/server.api/oauth/access_token'
 
+    
+    def oauth_request(self, token, url, extra_params=None):
+        request = ConsumerBasedOAuth.oauth_request(self, token, url, extra_params=extra_params)
+        request.pop("oauth_body_hash")
 
+        #consumer_secret = "a781b6c7f9b44255a2acbb70b914fbe1"
+        #request["oauth_signature"] = self._get_oauth_signature(request, consumer_secret, token)
+        return request
+
+    """
+    def _get_oauth_signature(self, request, consumer_secret, token):
+        from oauth2 import Token, escape
+        import hmac
+        import binascii
+        try:
+            from hashlib import sha1
+            sha = sha1
+        except ImportError:
+            # hashlib was added in Python 2.5
+            import sha
+        
+        if not hasattr(request, 'normalized_url') or request.normalized_url is None:
+            raise ValueError("Base URL for request is not set.")
+
+        sig = (
+            escape(request.method),
+            escape(request.normalized_url),
+            escape(request.get_normalized_parameters()),
+        )
+
+        key = '%s&' % escape(consumer_secret)
+        if token:
+            key += escape(token.secret)
+        raw = ' & '.join(sig)
+        
+        hashed = hmac.new(key, raw, sha)
+
+        # Calculate the digest base 64.
+        return binascii.b2a_base64(hashed.digest())[:-1]
+    """
+    
 # Backend definition
 BACKENDS = {
     'fatsecret': FatsecretAuth,
