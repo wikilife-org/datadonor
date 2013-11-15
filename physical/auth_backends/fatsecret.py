0b82e10c81c944cbbc007425ced42c16b57c5075
Joaquin Quintas <joako84@gmail.com>
2013-11-15 10:57:54 -0300

    FS

--- physical/auth_backends/fatsecret.py
+++ physical/auth_backends/fatsecret.py
@@ -5,6 +5,9 @@ Fatsecret OAuth 1.0 http://oauth.net/core/1.0/ flow
 http://platform.fatsecret.com/api/Default.aspx?screen=rapiauth#correctly_signing
 """
 
+from oauth2 import Consumer as OAuthConsumer, Token, Request as OAuthRequest, \
+                   SignatureMethod_HMAC_SHA1, HTTP_METHOD, Client, SignatureMethod_PLAINTEXT
+                   
 from social_auth.backends import OAuthBackend, PhysicalBackend, BaseOAuth,\
     ConsumerBasedOAuth
 
@@ -33,15 +36,37 @@ class FatsecretAuth(ConsumerBasedOAuth):
     REQUEST_TOKEN_URL = 'http://www.fatsecret.com/oauth/request_token'
     ACCESS_TOKEN_URL = 'http://platform.fatsecret.com/rest/server.api/oauth/access_token'
 
-    
     def oauth_request(self, token, url, extra_params=None):
-        request = ConsumerBasedOAuth.oauth_request(self, token, url, extra_params=extra_params)
+        extra_params["method"] = "profile.request_script_session_key"
+        params = {'oauth_callback': self.redirect_uri}
+        if extra_params:
+            params.update(extra_params)
+        
+        oauth_verifier = self.data.get('oauth_verifier')
+        if oauth_verifier:
+            params['oauth_verifier'] = oauth_verifier
+    
+        consumer = OAuthConsumer(*self.get_key_and_secret())
+        request = OAuthRequest.from_consumer_and_token(consumer,
+                                                       token=token,
+                                                       http_method="GET",
+                                                       http_url=url,
+                                                       parameters=params,
+                                                       is_form_encoded=True)
+        request.sign_request(SignatureMethod_HMAC_SHA1(), consumer, token)
+        return request
+    
+    def oauth_request_(self, token, url, extra_params=None):
+        #extra_params["method"] = "profile.request_script_session_key"
+
         request.pop("oauth_body_hash")
 
         #consumer_secret = "a781b6c7f9b44255a2acbb70b914fbe1"
         #request["oauth_signature"] = self._get_oauth_signature(request, consumer_secret, token)
+        #request["method"]= "profile.request_script_session_key"
         return request
 
+    
     """
     def _get_oauth_signature(self, request, consumer_secret, token):
         from oauth2 import Token, escape
