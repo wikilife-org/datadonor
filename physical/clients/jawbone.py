546d45c0aaf13a0680ec4d33be0cb6c68d3e98eb
Joaquin Quintas <joako84@gmail.com>
2014-04-04 00:14:27 -0300

    Jawbone

---
+++ physical/clients/jawbone.py
@@ -0,0 +1,102 @@
+import requests
+from physical.clients.base_device_client import BaseDeviceClient
+
+
+class JawboneClient(BaseDeviceClient):
+    def __init__(self, authorization_token):
+        self.username = '@me'
+        self.token = authorization_token
+        self.auth_header = {'Authorization': 'Bearer %s' % self.token}
+        self.content_header = {'Content-Type': 'multipart/form-data'}
+        self.version = 'v.1.0/'
+        self.api_url_base = 'https://jawbone.com/nudge/api/%s' % self.version
+
+
+    # USER REQUESTS
+    @property
+    def user_request_url_base(self):
+        return 'https://jawbone.com/nudge/api/v.1.1/users/%s' % self.username
+
+    def get_user_information(self):
+        user_information_request = requests.get(self.user_request_url_base, headers=self.auth_header)
+        return user_information_request.json()
+
+    def get_user_friends(self):
+        user_friends_request = requests.get(self.user_request_url_base + '/friends', headers=self.auth_header)
+        return user_friends_request.Response.json()
+
+    def get_user_mood(self):
+        user_mood_request = requests.get(self.user_request_url_base + '/mood', headers=self.auth_header)
+        return user_mood_request.json()
+
+    def get_user_trends(self):
+        user_trends_request = requests.get(self.user_request_url_base + '/trend', headers=self.auth_header)
+        return user_trends_request.json()
+
+    def get_user_moves(self):
+        user_moves_request = requests.get(self.user_request_url_base + '/moves', headers=self.auth_header)
+        return user_moves_request.json()
+
+    def get_user_workouts(self):
+        user_workouts_request = requests.get(self.user_request_url_base + '/workouts', headers=self.auth_header)
+        return user_workouts_request.json()
+
+    def get_user_sleep_list(self):
+        user_sleep_list_request = requests.get(self.user_request_url_base + '/sleeps', headers=self.auth_header)
+        return user_sleep_list_request.json()
+
+    def get_user_meals(self):
+        user_sleep_list_request = requests.get(self.user_request_url_base + '/meals', headers=self.auth_header)
+        return user_sleep_list_request.json()
+
+
+    # MOVES
+    def get_specific_move(self, move_xid):
+        specific_move_request = requests.get(self.api_url_base + 'moves/' + '{%s}' % move_xid, headers=self.auth_header)
+        return specific_move_request.json()
+
+    def get_move_graphs(self, move_xid):
+        move_graphs_request = requests.get(self.api_url_base + 'moves/' + '{%s}' + '/image' % move_xid, headers=self.auth_header)
+        return move_graphs_request.json()
+
+    def get_move_intensity(self, move_xid):
+        move_intensity_request = requests.get(self.api_url_base + 'moves/' + '{%s}' + '/snapshot' % move_xid, headers=self.auth_header)
+        return move_intensity_request.json()
+
+    #  WORKOUTS
+    def get_specific_workout(self, workout_xid):
+        specific_workout_request = requests.get(self.api_url_base + 'workouts/' + '{%s}' % workout_xid, headers=self.auth_header)
+        return specific_workout_request.json()
+
+    def get_user_workout_graphs(self, workout_xid):
+        workout_graphs_request = requests.get(self.api_url_base + 'workouts/' + '{%s}' + '/image' % workout_xid, headers=self.auth_header)
+        return workout_graphs_request.json()
+
+    def get_workout_intensity(self, workout_xid):
+        workout_intensity_request = requests.get(self.api_url_base + 'workouts/' + '{%s}' + '/snapshot' % workout_xid, headers=self.auth_header)
+        return workout_intensity_request.json()
+
+    #SLEEP
+    def get_specific_sleep(self, sleep_xid):
+        specific_workout_request = requests.get(self.api_url_base + 'sleeps/' + '{%s}' % sleep_xid, headers=self.auth_header)
+        return specific_workout_request.json()
+
+    def get_user_workout_graphs(self, sleep_xid):
+        sleep_graphs_request = requests.get(self.api_url_base + 'sleeps/' + '{%s}' + '/image' % sleep_xid, headers=self.auth_header)
+        return sleep_graphs_request.json()
+
+    def get_workout_intensity(self, sleep_xid):
+        sleep_phases_request = requests.get(self.api_url_base + 'sleeps/' + '{%s}' + '/snapshot' % sleep_xid, headers=self.auth_header)
+        return sleep_phases_request.json()
+
+    #MEALS
+    def get_specific_meal(self, meal_xid):
+        specific_meal_request = requests.get(self.api_url_base + 'meals/' + '{%s}' % meal_xid, headers=self.auth_header)
+        return specific_meal_request.json()
+
+    def post_user_meal(self, meal):
+        data = {'meal': meal}
+        post_headers = dict(self.content_header.items() + self.auth_header.items())
+        request_url = self.api_url_base + 'meals'
+        user_meal_post = requests.post(request_url, data=data, headers=post_headers)
+        return user_meal_post.text
---
+++ physical/services/jawbone_service.py
@@ -0,0 +1,113 @@
+# coding=utf-8
+
+from physical.clients.jawbone import JawboneClient
+from physical.services.base_device_service import BaseDeviceService
+
+from string import lower
+from physical.models import UserActivityLog
+from health.models import UserSleepLog
+from datetime import datetime
+from django.contrib.auth.models import User
+from nutrition.models import UserFoodLog
+
+JAWBONE_API = "https://api.fitbit.com"
+ACTIVITY_TYPE_NODE_ID_MAP = {
+    "Running": 0, 
+    "Cycling": 0, 
+    "Mountain Biking": 0, 
+    "Walking": 0, 
+    "Hiking": 0, 
+    "Downhill Skiing": 0, 
+    "Cross-Country Skiing": 0, 
+    "Snowboarding": 0, 
+    "Skating": 0, 
+    "Swimming": 0, 
+    "Wheelchair": 0, 
+    "Rowing": 0, 
+    "Elliptical": 0, 
+    "Other": 0
+}
+
+METERS_TO_MILES = 0.000621371192
+MILISECONDS_TO_HOURS =  3600000
+MILES_TO_STEPS = 2300
+
+class JawboneService(BaseDeviceService):
+
+    _profile_source = "jawbone"
+    def pull_user_info(self, user_id, user_auth):
+        pass
+    
+    def pull_user_info_(self, user_id, user_auth):
+        client = JawboneClient(JAWBONE_API,user_auth["access_token"])
+        profile = client.get_user_profile()["user"]
+        profile_items = {}
+        
+        if "gender" in profile:
+            if "male" == lower(profile["gender"]):
+                profile_items["gender"] = "m"
+            elif "female" == lower(profile["gender"]):
+                profile_items["gender"] = "f"
+                
+
+
+        """
+        if "location" in profile:
+            profile_items["location"] = profile["location"]
+        """
+
+        user = User.objects.get(id=user_id)
+        self._update_profile(user, **profile_items)
+
+        foods = client.get_user_foods()
+        for item in foods:
+            for food in item["foods"]:
+                food_entry_id = food["logId"]
+                fdate = food["logDate"]
+                carbs = food["nutritionalValues"]["carbs"]
+                protein = food["nutritionalValues"]["protein"]
+                fat = food["nutritionalValues"]["fat"]
+                fiber = food["nutritionalValues"]["fiber"]
+                
+                food_log, created = UserFoodLog.objects.get_or_create(user=user, device_log_id=food_entry_id, provider=self._profile_source)
+                food_log.provider = self._profile_source
+                food_log.device_log_id = food_entry_id
+                food_log.protein = float(protein)
+                food_log.carbs = float(carbs)
+                food_log.fat = float(fat)
+                food_log.fiber = float(fiber)
+                food_log.execute_time = datetime.strptime(fdate, '%Y-%m-%d')
+                food_log.save()
+                
+        sleeps = client.get_user_sleep()
+        for item in sleeps:
+            for sleep in item["sleep"]:
+                activity, created = UserSleepLog.objects.get_or_create(user=user, device_log_id=sleep["logId"])
+                
+                activity.execute_time = datetime.strptime(sleep["startTime"][:10], '%Y-%m-%d')
+                activity.provider = "fitbit"
+                activity.minutes = round(float(sleep["duration"]/ 60000),2)  
+                activity.save()
+                 
+        activities = client.get_user_fitness_activities()
+        for item in activities:
+            for activity in item["activities"]:
+
+                activity_obj, created = UserActivityLog.objects.get_or_create(user=user, device_log_id=activity["logId"])
+                activity_obj.type = activity["name"].lower()
+                
+                activity_obj.execute_time = datetime.strptime(activity["startDate"], '%Y-%m-%d')
+                activity_obj.provider = "fitbit"
+                
+                if "duration" in activity:
+                    activity_obj.hours = round(float(activity["duration"]) / MILISECONDS_TO_HOURS,2)
+                if "distance" in activity:
+                    activity_obj.miles =  round(float(activity["distance"]),2) 
+                if "steps" in activity:
+                    activity_obj.steps =  round(float(activity["steps"])) 
+
+                activity_obj.save()
+
+        
+    def pull_user_activity(self, user_id, user_auth):
+        pass
