a6a70565f942afd6242b3d68df4639214dce1e32
Joaquin Quintas <joako84@gmail.com>
2014-05-21 16:29:33 -0300

    Report

--- reports/services/internal.py
+++ reports/services/internal.py
@@ -9,7 +9,7 @@ def get_new_users_distribution():
     day_list = get_days_list(8)
     for day in day_list:
         
-        values = User.objects.filter(date_joined__day=day).count()
+        values = User.objects.filter(date_joined__year=day.strftime("%Y"), date_joined__month=day.strftime("%m"), date_joined__day=day.strftime("%d")).count()
         d_index = day.strftime("%b %d")
         result.append([d_index, values])
     print result
