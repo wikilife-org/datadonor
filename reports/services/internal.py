c0c3c0d2fe07198a2d867aacd0ac3f230a3c10a7
Joaquin Quintas <joako84@gmail.com>
2014-05-21 16:42:34 -0300

    Report

--- reports/services/internal.py
+++ reports/services/internal.py
@@ -5,11 +5,13 @@ from django.db.models.aggregates import Sum, Avg
 
 def get_new_users_distribution():
     result = []
-    result.append( ['Date', 'New Users'])
+    
     day_list = get_days_list(8)
     for day in day_list:
         
         values = User.objects.filter(date_joined__year=day.strftime("%Y"), date_joined__month=day.strftime("%m"), date_joined__day=day.strftime("%d")).count()
         d_index = day.strftime("%b %d")
         result.append([d_index, values])
-    return result.reverse()
+    result =  result.reverse()
+    result.insert( ['Date', 'New Users'])
+    return result
