6e9ab941e5e55ad167995fdf75c48752435fa455
root <root@datadonor.wikilife.org>
2014-09-10 21:00:46 +0000

    xls

--- update_user_date.sh
+++ update_user_date.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export PYTHONPATH=$PYTHONPATH:$PWD;
-export PYTHONPATH=$PYTHONPATH:$PWD/../wikilife_utils;
+export PYTHONPATH=$PYTHONPATH:/usr/local/datadonor_env/lib/python2.7/site-packages:/home/datadonor:/home/wikilife_utils;
 
 echo "Datadonors getting user information from devices ...";
 
+cd /home/datadonor
 python manage.py get_user_data
