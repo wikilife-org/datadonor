ff5730559724206e8004ec3abc04dfc4bc7be468
jquintas <jquintas@wikilife.org>
2015-02-06 16:59:07 -0300

    pic variable

--- users/management/commands/tweet_stats.py
+++ users/management/commands/tweet_stats.py
@@ -9,6 +9,10 @@ from health.utilities import get_conditions_rank, get_complaints_rank, get_emoti
 
 
 class Command(BaseCommand):
+    def url_screengrab(url, name, **kwargs):
+        cmd = "export DISPLAY=:0;/usr/local/bin/CutyCapt  --auto-load-images=on --delay=15000 --max-wait=60000  --url={u} --out=/home/datadonor/static/tmp/{name}.png".format(u = url, name=name)
+        os.system(cmd)
+    
         
     def handle(self, *args, **options):
         
@@ -19,48 +23,55 @@ class Command(BaseCommand):
             dto = PhysicalActivityDistributionService().get_steps_distribution_global()
             text = "%s #datadonors took an average of %s #steps this week  #activity #physical" %(dto["total_users"], dto["avg"])
             url = "http://datadonors.org/statistics/physical-activity-steps/"
-            tw.share_stat(text, url)
+            url_screengrab(url+"?pic=true", "physical-activity-steps")
+            tw.share_stat(text, url, "physical-activity-steps.png")
         
         if "miles" in args:
             print ("Tweet: Miles report")
             dto = PhysicalActivityDistributionService().get_miles_distribution_global()
             text = "%s #datadonors moved an average of %s #miles this week  #activity #physical" %(dto["total_users"], dto["avg"])
             url = "http://datadonors.org/statistics/physical-activity-miles/"
-            tw.share_stat(text, url)
+            url_screengrab(url+"?pic=true", "physical-activity-miles")
+            tw.share_stat(text, url, "physical-activity-miles.png")
         
         if "education" in args:
             print ("Tweet: Education report")
             text = "#datadonors #education level reached"
             url = "http://datadonors.org/statistics/social-education-level/"
-            tw.share_stat(text, url)
+            url_screengrab(url+"?pic=true", "social-education-level")
+            tw.share_stat(text, url, "social-education-level.png")
         
         if "work" in args:
             print ("Tweet: Work report")
             global_data, avg, total_users  = global_work()
             text = "#datadonors #working experience #avg %s years based on %s users"%(avg, total_users)
             url = "http://datadonors.org/statistics/social-work-years/"
-            tw.share_stat(text, url)
+            url_screengrab(url+"?pic=true", "social-work-years")
+            tw.share_stat(text, url, "social-work-years.png")
     
         if "complaints" in args:
             print ("Tweet: Complaints report")
             data, total = get_complaints_rank()
             text = "#datadonors #top #complaints based on %s users"%(total)
             url = "http://datadonors.org/statistics/social-health-complaints/"
-            tw.share_stat(text, url)
+            url_screengrab(url+"?pic=true", "social-health-complaints")
+            tw.share_stat(text, url, "social-health-complaints.png")
         
         if "conditions" in args:
             print ("Tweet: Conditions report")
             data, total = get_conditions_rank()
             text = "#datadonors #top #conditions based on %s users"%(total)
             url = "http://datadonors.org/statistics/social-health-conditions/"
-            tw.share_stat(text, url)
+            url_screengrab(url+"?pic=true", "social-health-conditions")
+            tw.share_stat(text, url, "social-health-conditions.png")
         
         if "emotions" in args:
             print ("Tweet: Emotions report")
             data, total = get_emotions_rank()
             text = "#datadonors #top #emotions based on %s users"%(total)
             url = "http://datadonors.org/statistics/social-health-emotions/"
-            tw.share_stat(text, url)
+            url_screengrab(url+"?pic=true", "social-health-emotions")
+            tw.share_stat(text, url, "social-health-emotions.png")
                     
              
 class TwitterService():
@@ -71,7 +82,7 @@ class TwitterService():
         self.url_separator = " "
         self.url_separator_trimmed = "... "
 
-    def share_stat(self, text, url):
+    def share_stat(self, text, url, img_name):
         oauth_token = "117759178-JJqLuTIR0GYYZXcbWfpV564BjfJJbWO57vatqSgF"
         oauth_secret = "vwG1K54ejtE9ahpr7b9VxbxPu1T7iMzkFzIshAQVZcgg8"
 
@@ -85,17 +96,22 @@ class TwitterService():
         # from help/configuration#short_url_length_https
         url_length = len(url)
         # from help/configuration#characters_reserved_per_media
-
+        image_file = "/home/datadonor/static/tmp/{name}".format(name=img_name)
+        
         if len(text) + url_length  > 140:
             trim_to = 140 - url_length  \
                 - len(self.url_separator_trimmed)
             text = text[:trim_to] + self.url_separator_trimmed + url
         else:
             text = text + self.url_separator + url
-
+        
+        imagefile= open(image_file, "rb")
+        data = imagefile.read()
+            
         params = {
-                 # "media[]": url,
+            "media[]": data,
             "status": text,
         }
- 
-        twitter.statuses.update(**params)
+        
+        twitter.statuses.update_with_media(**params)
+        
